58e42524211f39b4c60b70538314b073bbe329ca
JustinLung <justinlung77@gmail.com>
2022-03-28 12:09:22 +0200

    :tada: initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//Routes
+const indexRoute = require('./routes/index')
+
+app.set('views', path.join(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', indexRoute)
+
+app.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
