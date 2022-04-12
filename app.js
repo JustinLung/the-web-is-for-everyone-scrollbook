f3d55e0ab4a18b447cd5e9e91ac3a9c1e24cbaa7
JustinLung <justinlung77@gmail.com>
2022-04-12 11:55:53 +0200

    :sparkles: added 404 route

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.use('/', indexRoute)
 app.use('/book', detailRoute)
 
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404).render('error')
+})
+
 app.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
