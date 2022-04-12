5f79cb8e8194da6b809c2ea57b0e999c1b92232d
JustinLung <justinlung77@gmail.com>
2022-04-12 12:59:03 +0200

    :sparkles: about page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 
 //Routes
 const indexRoute = require('./routes/index')
+const aboutRoute = require('./routes/about')
 const detailRoute = require('./routes/detail')
 
 app.set('views', path.join(__dirname + '/views'))
@@ -13,10 +14,11 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 app.use('/', indexRoute)
 app.use('/book', detailRoute)
+app.use('/about', aboutRoute)
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  res.status(404).render('error')
-})
+// app.use(function (req, res, next) {
+//   res.status(404).render('error')
+// })
 
 app.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
