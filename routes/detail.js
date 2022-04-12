24e81127d860f811e7da81aa2c9b88873a74cd43
JustinLung <justinlung77@gmail.com>
2022-04-12 16:02:10 +0200

    :art: removed unused import function

--- routes/detail.js
+++ routes/detail.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getBookById, getAllBooks } = require('./../modules/api')
+const { getBookById } = require('./../modules/api')
 
 router
 
